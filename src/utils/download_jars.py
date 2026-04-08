573d41040298683a4d431e89e05e7e7f48fe386b
thethien8a <thethien1708@gmail.com>
2026-04-08 09:26:33 +0700

    feat: prometheus for logging control

--- src/utils/download_jars.py
+++ src/utils/download_jars.py
@@ -6,7 +6,9 @@ JARS = [
     "https://repo1.maven.org/maven2/org/projectnessie/nessie-integrations/nessie-spark-extensions-3.5_2.12/0.99.0/nessie-spark-extensions-3.5_2.12-0.99.0.jar",
     "https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-aws/3.3.4/hadoop-aws-3.3.4.jar",
     "https://repo1.maven.org/maven2/com/amazonaws/aws-java-sdk-bundle/1.12.262/aws-java-sdk-bundle-1.12.262.jar",
-    "https://repo1.maven.org/maven2/org/wildfly/openssl/wildfly-openssl/1.0.7.Final/wildfly-openssl-1.0.7.Final.jar"
+    "https://repo1.maven.org/maven2/org/wildfly/openssl/wildfly-openssl/1.0.7.Final/wildfly-openssl-1.0.7.Final.jar",
+    # JMX Exporter cho Prometheus monitoring (Spark, Trino)
+    "https://repo1.maven.org/maven2/io/prometheus/jmx/jmx_prometheus_javaagent/1.0.1/jmx_prometheus_javaagent-1.0.1.jar",
 ]
 
 def main():
